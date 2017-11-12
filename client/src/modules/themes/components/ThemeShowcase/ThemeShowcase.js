38be0b3c2a2e2cdc644cfcc5645a8e5395fc7604
Marcin Zolkiewski <zolkiewski.marcin@gmail.com>
2017-11-12 10:52:53 +0100

    simplified color themes

--- client/src/modules/themes/components/ThemeShowcase/ThemeShowcase.js
+++ client/src/modules/themes/components/ThemeShowcase/ThemeShowcase.js
@@ -3,23 +3,23 @@ import React from 'react';
 import styles from './ThemeShowcase.css';
 
 const groups = ['bg', 'primary', 'bg', 'secondary', 'bg', 'tertiary'];
-const variants = ['Lowest', 'Lower', '', 'Higher', 'Highest'];
+const variants = ['Dark', '', 'Light'];
 
 const ThemeShowcase = () => {
-  return groups.map((group) => {
+  return groups.map((group, i) => {
 
     const cols = variants.map((variant) => {
       // const texts = variants.map((variant) => <p key={variant} className={`${group}${variant}Text`}>text</p>);
       // const containers =
       return (
         <div key={group + variant} className={styles[`${group}${variant}`]}>
-          <p className={styles.text}>text</p>
+          <p className={styles[`${group}Text`]}>text</p>
         </div>
       );
     });
 
     return (
-      <div key={group} className={styles.row}>
+      <div key={group + i} className={styles.row}>
         {cols}
       </div>
     );
