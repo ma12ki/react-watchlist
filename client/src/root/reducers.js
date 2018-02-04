5dc97eb9d24d04281d64e0a1164edb522b9e68c5
Marcin Zolkiewski <zolkiewski.marcin@gmail.com>
2018-02-04 10:00:49 +0100

    renamed modules

--- client/src/root/reducers.js
+++ client/src/root/reducers.js
@@ -1,15 +1,15 @@
 import { moduleName as themesModuleName, reducers as themesReducers } from '../modules/themes';
 import { moduleName as screenModuleName, reducers as screenReducers } from '../modules/screen';
 import { moduleName as dashboardModuleName, reducers as dashboardReducers } from '../modules/dashboard';
-import { moduleName as episodeActionsModuleName, reducers as episodeActionsReducers } from '../modules/episodeActions';
+import { moduleName as userShowActionsModuleName, reducers as userShowActionsReducers } from '../modules/userShowActions';
 import { moduleName as showsModuleName, reducers as showsReducers } from '../modules/shows';
-import { moduleName as showEditModuleName, reducers as showEditReducers } from '../modules/showEdit';
+import { moduleName as showActionsModuleName, reducers as showActionsReducers } from '../modules/showActions';
 
 export default {
   [themesModuleName]: themesReducers,
   [screenModuleName]: screenReducers,
   [dashboardModuleName]: dashboardReducers,
-  [episodeActionsModuleName]: episodeActionsReducers,
+  [userShowActionsModuleName]: userShowActionsReducers,
   [showsModuleName]: showsReducers,
-  [showEditModuleName]: showEditReducers,
+  [showActionsModuleName]: showActionsReducers,
 };
