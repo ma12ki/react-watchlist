e335832dae99ffc0139d87e006b70d26f85e0c53
Marcin Zolkiewski <zolkiewski.marcin@gmail.com>
2018-02-05 12:50:01 +0100

    pushing watched episodes to bottom of list

--- client/src/modules/dashboard/calendar/components/Calendar/EpisodesList.js
+++ client/src/modules/dashboard/calendar/components/Calendar/EpisodesList.js
@@ -9,26 +9,36 @@ import { MarkWatchedButton, PostponeButton } from '../../../../showOperations';
 import styles from './EpisodesList.css';
 
 const EpisodesList = ({ episodes }) => {
-  return episodes.map(e => {
-    const title = `${e.title} ${episodeLabel(e.season, e.episode)}`;
+  return episodes
+    .sort((e1, e2) => {
+      if (e1.watched && !e2.watched) {
+        return 1;
+      }
+      if (!e1.watched && e2.watched) {
+        return -1;
+      }
+      return 0;
+    })
+    .map(e => {
+      const title = `${e.title} ${episodeLabel(e.season, e.episode)}`;
 
-    const content = (
-      <React.Fragment>
-        <MarkWatchedButton
-          showId={e.showId}
-          episodeId={e.episodeId}
-          watched={e.watched}
-          title={title}
-        />
-        <PostponeButton
-          showId={e.showId}
-          season={e.season}
-          episode={e.episode}
-          currentPremiereDate={e.premiereDate}
-          title={title}
-        />
-      </React.Fragment>
-    );
+      const content = (
+        <React.Fragment>
+          <MarkWatchedButton
+            showId={e.showId}
+            episodeId={e.episodeId}
+            watched={e.watched}
+            title={title}
+          />
+          <PostponeButton
+            showId={e.showId}
+            season={e.season}
+            episode={e.episode}
+            currentPremiereDate={e.premiereDate}
+            title={title}
+          />
+        </React.Fragment>
+      );
 
     return (
       <Popover
