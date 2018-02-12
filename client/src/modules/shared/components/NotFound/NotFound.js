24588bd861f1846bbf8fa1e64d0e058137f3e84b
Marcin Zolkiewski <zolkiewski.marcin@gmail.com>
2018-02-12 01:09:25 +0100

    improved 404 page

--- client/src/modules/shared/components/NotFound/NotFound.js
+++ client/src/modules/shared/components/NotFound/NotFound.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import CakeIcon from 'material-ui-icons/Cake';
 
-const NotFound = () => <div>404 ;///</div>;
+import H from '../H';
+import styles from './NotFound.css';
+
+const NotFound = () => (
+  <div className={styles.container}>
+    <H size="2">You found the secret 404 page!</H>
+    <H size="3">Here, have some cake:</H>
+    <CakeIcon className={styles.cake} />
+  </div>
+);
 
 export default NotFound;
