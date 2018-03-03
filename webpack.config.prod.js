0e475f44b52e860b2eb8b0042233a45f8dc040ff
Marcin Zolkiewski <zolkiewski.marcin@gmail.com>
2018-03-03 19:54:03 +0100

    fixed webpack config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -50,7 +50,10 @@ export default {
     new webpack.DefinePlugin(GLOBALS),
 
     // Generate an external css file with a hash in the filename
-    new ExtractTextPlugin('[name].[contenthash].css'),
+    new ExtractTextPlugin({
+      filename: '[name].[contenthash].css',
+      allChunks: true,
+    }),
 
     // Generate HTML file that contains references to generated bundles. See here for how this works: https://github.com/ampedandwired/html-webpack-plugin#basic-usage
     new HtmlWebpackPlugin({
