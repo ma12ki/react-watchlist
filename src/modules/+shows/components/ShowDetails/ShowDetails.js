880f7c47fe823116e57650013ac8078802a1bd10
Marcin Zolkiewski <zolkiewski.marcin@gmail.com>
2018-03-04 16:54:50 +0100

    fixed show details

--- src/modules/+shows/components/ShowDetails/ShowDetails.js
+++ src/modules/+shows/components/ShowDetails/ShowDetails.js
@@ -4,20 +4,28 @@ import { connect } from 'react-redux';
 
 import { Aka, Card, H, ShowTypeIcon } from '../../../shared';
 import { isAtLeastAdminSel } from '../../../user';
+import { payloadSel } from '../../../location';
 import { EditShowButton, FollowButton } from '../../../showOperations';
-import { showLoadingSel, showSel } from '../../duck';
+import { showLoadingSel, showSel, getShowRequest } from '../../duck';
 import EpisodesList from './EpisodesList';
 import PremiereDate from './PremiereDate';
 import styles from './ShowDetails.css';
 
 class ShowDetails extends React.Component {
+  componentDidMount() {
+    const { show, loading, slug } = this.props;
+    if (!loading && !show.showId) {
+      this.props.onLoadShow(slug);
+    }
+  }
+
   render() {
     const { show, loading, isAtLeastAdmin } = this.props;
     const { showId, title, aka, type, recurring, following, episodes } = show;
 
     return (
       <Card loading={loading} className={styles.card}>
-        {!loading && <div className={styles.content}>
+        {!loading && showId && <div className={styles.content}>
           <ShowTypeIcon type={type} size="xlarge" className={styles.showTypeIcon} />
           <div>
             <div>
@@ -45,12 +53,19 @@ ShowDetails.propTypes = {
   isAtLeastAdmin: PropTypes.bool.isRequired,
   loading: PropTypes.bool.isRequired,
   show: PropTypes.object.isRequired,
+  slug: PropTypes.string.isRequired,
+  onLoadShow: PropTypes.func.isRequired,
 };
 
-const mapState = (state) => ({
+const mapState = state => ({
   isAtLeastAdmin: isAtLeastAdminSel(state),
   loading: showLoadingSel(state),
   show: showSel(state),
+  slug: payloadSel(state).slug,
 });
 
-export default connect(mapState)(ShowDetails);
+const mapDispatch = dispatch => ({
+  onLoadShow: slug => dispatch(getShowRequest(slug)),
+});
+
+export default connect(mapState, mapDispatch)(ShowDetails);
